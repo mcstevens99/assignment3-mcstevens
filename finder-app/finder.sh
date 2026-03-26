689c26a0ff760ffc3ee9f0df8a00d7c906544212
mcstevens99 <cesteves@alumni.uminho.pt>
2026-03-26 15:48:08 +0000

    correct finder.sh conf folder

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -7,7 +7,7 @@ set -eu
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/onf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -31,7 +31,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat /etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
